f58fb853bce51ab80a90232297756c45b18aa675
Anshul Kansal <kansalanshul121@gmail.com>
2023-04-22 14:54:47 +0530

    Added Sort Colors

---
+++ Dutch National Flag Problem (Sort Colors).cpp
@@ -0,0 +1,33 @@
+// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+// You must solve this problem without using the library's sort function.
+
+// Example 1:
+// Input: nums = [2,0,2,1,1,0]
+// Output: [0,0,1,1,2,2]
+
+// Example 2:
+// Input: nums = [2,0,1]
+// Output: [0,1,2]
+ 
+// Constraints:
+// n == nums.length
+// 1 <= n <= 300
+// nums[i] is either 0, 1, or 2.
+
+//Solution
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int temp;
+        for(int i=0;i<nums.size();i++){
+            for(int j=1;j<nums.size();j++){
+                if(nums[j-1]>nums[j]){
+                    temp = nums[j-1];
+                    nums[j-1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+    }
+};
